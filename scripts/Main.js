c38e38450a3169ea78ca8fbfd99b88bade771573
unknown <dvdscott23@gmail.com>
2019-04-08 11:06:28 +0100

    server added http-server

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,6 +1,6 @@
 function main()
 {
-    var ws = new WebSocket("ws://localhost:3000/wstest");
+    //var ws = new WebSocket("ws://localhost:3000/wstest");
 
     const game = new Game();
     game.update();
--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,17 @@
-  const http = require('http');
-  const fs = require('fs');
-  var formidable = require('formidable');
+  //const http = require('http');
+  //const fs = require('fs');
 
-  const hostname = '127.0.0.1';
-  const port = 3000;
+  //const hostname = '127.0.0.1';
+  //const port = 3000;
 
-  //var myGame = require('./Game.js');
-  //eval(fs.readFileSync('Game.js')+'');
 
-  fs.readFile('../Blank.html',(err,html) => {if(err){throw err;}
-
-    const server = http.createServer((req,res)=>{
-      res.StatusCode = 200;
-      res.setHeader('Content-type', 'text/html');
-      res.write(html);
-      res.end();
-    });
-
-    server.listen(port, hostname, () => {
-      console.log('Server Started at Port' + port );
-    });
-
-  });
-
-  //fs.readFile(__dirname +'/Game.js',(err,js) => {if(err){throw err;}} );
+//  let server = http.createServer(function(req,res){
+//      console.log('Kicked');
+//      res.writeHeader(200,{'Content-type': 'text/plain'});
+//      res.write('Hello');
+//      let claw = fs.readFile('../Blank.html');
+//      res.end();
+//    });
+//    server.listen(port, hostname, () => {
+//      console.log('Server Started at Port ' + port );
+//    });
