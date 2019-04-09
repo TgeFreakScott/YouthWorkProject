a769a22c3ed0cb697e2e91748a7bac29dbef17c2
unknown <dvdscott23@gmail.com>
2019-04-09 12:10:35 +0100

    phaser with semi-working controller (updates only with mouse movement in browser)

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,63 +1,96 @@
 function main()
 {
-    //var ws = new WebSocket("ws://localhost:3000/wstest");
+
+  function iconChange()
+  {
+    setTimeout(function(){ document.getElementById("icon").href = "Sprite/frame_0.png";}, 1000);
+    setTimeout(function(){ document.getElementById("icon").href = "Sprite/frame_1.png";}, 2000);
+    setTimeout(function(){ document.getElementById("icon").href = "Sprite/frame_2.png";}, 3000);
+    setTimeout(function(){ document.getElementById("icon").href = "Sprite/frame_3.png";}, 4000);
+  }
 
     //const game = new Game();
     //game.update();
 
     var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create
+        type: Phaser.AUTO,
+        parent: 'phaser-example',
+        width: 800,
+        height: 600,
+        pixelArt: true,
+        input: {gamepad: true},
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var redSprite
+    var game = new Phaser.Game(config);
+
+    function preload()
+    {
+      this.load.image('blue', 'Sprite/pinkCapture.png');
+      this.load.image('red', 'Sprite/redCapture.png');
+      this.load.spritesheet('pink', 'Sprite/pinkJump.png', { frameWidth: 331, frameHeight: 294 });
+      setInterval(function() { iconChange();}, 10);
     }
-};
 
-var game = new Phaser.Game(config);
+    function create()
+    {
 
-function preload ()
-{
-    this.load.image('raster', 'Sprite/pinkCapture.png');
-}
+      var pinkAnimation = this.anims.create({
+          key: 'walk',
+          frames: this.anims.generateFrameNumbers('pink'),
+          frameRate: 6,
+          repeat: 100
+      });
 
-function create ()
-{
-    var group = this.add.group();
 
-    group.createMultiple({ key: 'raster', repeat: 64 });
+      var sprite = this.add.sprite(50, 300, 'pink').setScale(0.6);
+      sprite.play('walk');
 
-    var hsv = Phaser.Display.Color.HSVColorWheel();
+      this.tweens.add({targets: sprite,x: 750,duration: 8800,ease: 'Linear'});
 
-    var i = 0;
+      sprite.on('animationrepeat-walk', function () {
+          var blueSpawn = this.add.image(sprite.x - 32, 300, 'blue').setScale(0.2);
 
-    var _this = this;
+          this.tweens.add({targets: blueSpawn,
+            props: {x: {value: '-=64', ease: 'Power1'},
+            y: {value: '+=50', ease: 'Bounce.easeOut'}},duration: 750});},this);
 
-    group.children.iterate(function (child) {
+      redSprite = this.add.sprite(400, 300, 'red').setScale(0.5);
 
-        child.x = 400;
-        child.y = 100;
-        child.depth = 64 - i;
+    }
 
-        child.setTint(hsv[i * 4].color);
 
-        i++;
+    function update()
+    {
+      if (jumpButton.isDown)
+      {
+       // jump code goes here!
+       redSprite.x += 4;
+      }
+        if (this.input.gamepad.total === 0)
+        {
+            return;
+        }
 
-        _this.tweens.add({
-            targets: child,
-            props: {
-                y: { value: 500, duration: 1500 },
-                scaleX: { value: child.depth / 64, duration: 6000, hold: 2000, delay: 2000 }
-            },
-            yoyo: true,
-            repeat: -1,
-            ease: 'Sine.easeInOut',
-            delay: 32 * i
-        });
+        var pad = this.input.gamepad.getPad(0);
+        if (pad.axes.length)
+        {
+            var axisH = pad.axes[0].getValue();
+            var axisV = pad.axes[1].getValue();
 
-    });
-}
+            redSprite.x += 4 * axisH;
+            redSprite.y += 4 * axisV;
+
+            redSprite.flipX = (axisH > 0);
+        }
+
+    }
+
+
 
 }
