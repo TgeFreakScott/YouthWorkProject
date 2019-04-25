e2fed3cb35d46061ea45e57eb48a81fb1d470922
michael lyons <michaelyons5@gmail.com>
2019-04-25 13:00:20 +0100

    server.js started

---
+++ scripts/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/scripts',express.static(__dirname + '/scripts'));
+app.use('/Sprite',express.static(__dirname + '/Sprite'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/Blank.html');
+});
+
+server.listen(8081,function(){ // Listens to port 8081
+    console.log('Listening on '+server.address().port);
+});
