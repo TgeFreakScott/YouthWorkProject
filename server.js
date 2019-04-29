5126cecb29d21930b8095f4904b7366f6ec137f7
michael lyons <michaelyons5@gmail.com>
2019-04-29 12:08:29 +0100

    server works, yet to implement realtime movement

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/Blank.html');
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/scripts',express.static(__dirname + '/scripts'));
+app.use('/Sprite',express.static(__dirname + '/Sprite'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/Blank.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket)
+{
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+server.listen(8081,function(){ // Listens to port 8081
+    console.log('Listening on '+server.address().port);
 });
