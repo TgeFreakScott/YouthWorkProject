6af3b1eea9f1f2da061032afd6f0517fe03e3c83
michael lyons <michaelyons5@gmail.com>
2019-05-23 12:28:11 +0100

    movement and colours work, need to work on using matter physics.

--- test/scripts/game.js
+++ test/scripts/game.js
@@ -19,13 +19,13 @@ var config = {
             }
     };
     var players = [];
-    var game = new Phaser.Game(config);
     var greySprite;
     var blueSprite;
     var pinkSprite;
     var greenSprite;
     var yellowSprite;
-    var numberofPlayersConnected = 0;
+    var game = new Phaser.Game(config);
+    const MAXPLAYERS = 5;
     function preload()
         {
           this.load.image('pink', 'Sprite/pinkCapture.png', 'Sprite/physics/pinkShape.json');
@@ -38,23 +38,31 @@ var config = {
           this.load.json('greenShape', 'Sprite/physics/greenShape.json');
           this.load.image('yellow', 'Sprite/yellowCapture.png', 'Sprite/physics/yellowShape.json');
           this.load.json('yellowShape', 'Sprite/physics/yellowShape.json');
+          this.load.image('white', 'Sprite/whiteCapture.png', 'Sprite/physics/pinkShape.json')
         }
         function create()
         {
           var self = this;
           this.socket = io();
+          this.otherPlayers = this.physics.add.group();
           cursors = this.input.keyboard.createCursorKeys();
-          keys = this.input.keyboard.addKeys('W,A,S,D,I,J,K,L');
+          keys = this.input.keyboard.addKeys('W,A,S,D');
           this.socket.on('currentPlayers', function (players)
           {
             Object.keys(players).forEach(function (id)
             {
-              addPlayer(self, players[id]);
+              if (players[id].playerId === self.socket.id)
+              {
+                addPlayer(self, players[id]);
+              } else
+              {
+                addOtherPlayers(self, players[id]);
+              }
             });
           });
           this.socket.on('newPlayer', function (playerInfo)
           {
-            addPlayer(self, playerInfo);
+            addOtherPlayers(self, playerInfo);
           });
           this.socket.on('disconnect', function (playerId)
           {
@@ -65,40 +73,20 @@ var config = {
                 otherPlayer.destroy();
               }
             });
-          });
-
-          this.socket.on('PlayerCount', function(playersConnected)
-          {
-            numberofPlayersConnected = playersConnected;
-          });
-
+          })
           this.socket.on('playerMoved', function (playerInfo)
           {
-            if (playerInfo.playerId === pinkSprite.playerId)
+            self.otherPlayers.getChildren().forEach(function (otherPlayer)
             {
-              pinkSprite.setPosition(playerInfo.x, playerInfo.y);
-            }
-            if (playerInfo.playerId === blueSprite.playerId)
-            {
-              blueSprite.setPosition(playerInfo.x, playerInfo.y);
-            }
-            if (playerInfo.playerId === greySprite.playerId)
-            {
-              greySprite.setPosition(playerInfo.x, playerInfo.y);
-            }
-            if (playerInfo.playerId === greenSprite.playerId)
-            {
-              greenSprite.setPosition(playerInfo.x, playerInfo.y);
-            }
-            if (playerInfo.playerId === yellowSprite.playerId)
-            {
-              yellowSprite.setPosition(playerInfo.x, playerInfo.y);
-            }
+              if (playerInfo.playerId === otherPlayer.playerId)
+              {
+                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+              }
+            });
           });
           this.cursors = this.input.keyboard.createCursorKeys();
           this.matter.world.setBounds();
           this.input.setPollAlways();
-
         }
 
         function update()
@@ -114,180 +102,89 @@ var config = {
 
                 //this.ship.flipX = (pinkAxisH < 0);
             //}
-            if (pinkSprite)
+            if (this.player)
             {
               if (keys.A.isDown)
               {
-                pinkSprite.x --;
+                this.player.x --;
               }
               else if (keys.D.isDown)
               {
-                pinkSprite.x ++;
+                this.player.x ++;
               }
-
               if (keys.W.isDown)
               {
-                pinkSprite.y --;
+                this.player.y --;
               }
               else if (keys.S.isDown)
               {
-                pinkSprite.y ++;
+                this.player.y ++;
               }
-              var pinkX = pinkSprite.x;
-              var pinkY = pinkSprite.y;
-              if (pinkSprite.oldPosition && (pinkX !== pinkSprite.oldPosition.x || pinkY !== pinkSprite.oldPosition.y))
-              {
-                this.socket.emit('playerMovement', { x: pinkSprite.x, y: pinkSprite.y});
-              }
-
-              // save old position data
-              pinkSprite.oldPosition =
-              {
-                x: pinkSprite.x,
-                y: pinkSprite.y
-              };
             }
-
-            if(blueSprite)
+            // emit player movement
+            var x = this.player.x;
+            var y = this.player.y;
+            if (this.player.oldPosition && (x !== this.player.oldPosition.x || y !== this.player.oldPosition.y))
             {
-              if (keys.J.isDown)
-              {
-                blueSprite.x --;
-              }
-              else if (keys.L.isDown)
-              {
-                blueSprite.x ++;
-              }
-              if (keys.I.isDown)
-              {
-                blueSprite.y --;
-              }
-              else if (keys.K.isDown)
-              {
-                blueSprite.y ++;
-              }
-              var blueX = blueSprite.x;
-              var blueY = blueSprite.y;
-              if (blueSprite.oldPosition && (blueX !== blueSprite.oldPosition.x || blueY !== blueSprite.oldPosition.y))
-              {
-                this.socket.emit('playerMovement', { x: blueSprite.x, y: blueSprite.y});
-              }
-
-              // save old position data
-              blueSprite.oldPosition =
-              {
-                x: blueSprite.x,
-                y: blueSprite.y
-              };
+              this.socket.emit('playerMovement', { x: this.player.x, y: this.player.y});
             }
 
-            if(greySprite)
-            {
-              if (cursors.left.isDown)
-              {
-                greySprite.x --;
-              }
-              else if (cursors.right.isDown)
-              {
-                greySprite.x ++;
-              }
-
-              if (cursors.up.isDown)
-              {
-                greySprite.y --;
-              }
-              else if (cursors.down.isDown)
-              {
-                greySprite.y ++;
-              }
-              var greyX = greySprite.x;
-              var greyY = greySprite.y;
-              if (greySprite.oldPosition && (greyX !== greySprite.oldPosition.x || greyY !== greySprite.oldPosition.y))
-              {
-                this.socket.emit('playerMovement', { x: greySprite.x, y: greySprite.y});
-              }
-
-              // save old position data
-              greySprite.oldPosition =
-              {
-                x: greySprite.x,
-                y: greySprite.y
-              };
-            }
-
-            if(greenSprite)
-            {
-              var greenX = greenSprite.x;
-              var greenY = greenSprite.y;
-              if (greenSprite.oldPosition && (greenX !== greenSprite.oldPosition.x || greenY !== greenSprite.oldPosition.y))
-              {
-                this.socket.emit('playerMovement', { x: greenSprite.x, y: greenSprite.y});
-              }
-
-              // save old position data
-              greenSprite.oldPosition =
-              {
-                x: greenSprite.x,
-                y: greenSprite.y
-              };
-            }
-
-            if(yellowSprite)
-            {
-              var yellowX = yellowSprite.x;
-              var yellowY = yellowSprite.y;
-              if (yellowSprite.oldPosition && (yellowX !== yellowSprite.oldPosition.x || yellowY !== yellowSprite.oldPosition.y))
-              {
-                this.socket.emit('playerMovement', { x: yellowSprite.x, y: yellowSprite.y});
-              }
-
-              // save old position data
-              yellowSprite.oldPosition =
-              {
-                x: yellowSprite.x,
-                y: yellowSprite.y
-              };
-            }
+            // save old position data
+            this.player.oldPosition = {
+              x: this.player.x,
+              y: this.player.y
+            };
           }
+
           function addPlayer(self, playerInfo)
           {
-            switch(playerInfo.playerNumber)
+            self.player = self.physics.add.image(playerInfo.x, playerInfo.y, 'white').setScale(0.2);
+
+            if (playerInfo.playerNumber === 0)
             {
-              case 0:
-                pinkSprite = self.physics.add.image(playerInfo.x, playerInfo.y, 'pink').setScale(0.2).setCollideWorldBounds(true);
-                pinkSprite.playerId = playerInfo.playerId;
-                break;
-              case 1:
-                blueSprite = self.physics.add.image(playerInfo.x, playerInfo.y, 'blue').setScale(0.2).setCollideWorldBounds(true);
-                blueSprite.playerId = playerInfo.playerId;
-                break;
-              case 2:
-                greySprite = self.physics.add.image(playerInfo.x, playerInfo.y, 'grey').setScale(0.2).setCollideWorldBounds(true);
-                greySprite.playerId = playerInfo.playerId;
-                break;
-              case 3:
-                greenSprite = self.physics.add.image(playerInfo.x, playerInfo.y, 'green').setScale(0.2).setCollideWorldBounds(true);
-                greenSprite.playerId = playerInfo.playerId;
-                break;
-              case 4:
-                yellowSprite = self.physics.add.image(playerInfo.x, playerInfo.y, 'yellow').setScale(0.2).setCollideWorldBounds(true);
-                yellowSprite.playerId = playerInfo.playerId;
-                break;
-              case 5:
-                break;
-              case 6:
-                break;
-              case 7:
-                break;
-              default:
-                break;
+              self.player.setTint(0xff1493);
+            }
+            else if(playerInfo.playerNumber === 1)
+            {
+              self.player.setTint(0x3338ff);
+            }
+            else if(playerInfo.playerNumber === 2)
+            {
+              self.player.setTint(0x757575);
+            }
+            else if(playerInfo.playerNumber === 3)
+            {
+              self.player.setTint(0x00ae1a);
+            }
+            else if(playerInfo.playerNumber === 4)
+            {
+              self.player.setTint(0xdbff00);
             }
           }
-
           function addOtherPlayers(self, playerInfo)
           {
-            var shapePink = self.cache.json.get('blueShape');
-            const otherPlayer = self.physics.add.image(playerInfo.x, playerInfo.y, 'blue').setScale(0.2).setCollideWorldBounds(true);
+            const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'white').setScale(0.2);
+
+            if (playerInfo.playerNumber === 0)
+            {
+              otherPlayer.setTint(0xff1493);
+            }
+            else if(playerInfo.playerNumber === 1)
+            {
+              otherPlayer.setTint(0x3338ff);
+            }
+            else if(playerInfo.playerNumber === 2)
+            {
+              otherPlayer.setTint(0x757575);
+            }
+            else if(playerInfo.playerNumber === 3)
+            {
+              otherPlayer.setTint(0x00ae1a);
+            }
+            else if(playerInfo.playerNumber === 4)
+            {
+              otherPlayer.setTint(0xdbff00);
+            }
             otherPlayer.playerId = playerInfo.playerId;
             self.otherPlayers.add(otherPlayer);
           }
